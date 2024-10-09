db18f1d89f3c800034e8f9105c171d2d7b6f035f
JisanAR <altafurrahaman3@gmail.com>
2024-10-09 06:26:32 +0600

    update the docker file for match with changes

--- app/core/cloudflare_storage.py
+++ app/core/cloudflare_storage.py
@@ -1,5 +1,5 @@
-import boto3
-from botocore.client import Config
+import aioboto3
+from botocore.config import Config
 from typing import Optional, IO, Tuple
 import uuid
 
@@ -16,23 +16,28 @@ class CloudflareR2Storage:
         self.secret_access_key = config.R2_SECRET_ACCESS_KEY
         self.bucket_name = config.R2_BUCKET_NAME
 
-        self.s3_client = boto3.client(
-            's3',
-            endpoint_url=self.endpoint_url,
-            aws_access_key_id=self.access_key_id,
-            aws_secret_access_key=self.secret_access_key,
-            config=Config(signature_version='s3v4'),
-        )
+        self.session = aioboto3.Session()
 
-    async def upload_image(self, user_uid: str, image_id: uuid.UUID, file_obj: IO) -> Optional[Tuple[str, str]]:
+        self.client_config = Config(signature_version='s3v4')
+
+    async def upload_image(
+        self, user_uid: str, image_id: uuid.UUID, file_obj: IO[bytes]
+    ) -> Optional[Tuple[str, str]]:
         key = f"images/{user_uid}/{image_id}.jpg"
         try:
-            self.s3_client.upload_fileobj(
-                Fileobj=file_obj,
-                Bucket=self.bucket_name,
-                Key=key,
-                ExtraArgs={'ContentType': 'image/jpeg'}
-            )
+            async with self.session.client(
+                's3',
+                endpoint_url=self.endpoint_url,
+                aws_access_key_id=self.access_key_id,
+                aws_secret_access_key=self.secret_access_key,
+                config=self.client_config,
+            ) as s3_client:
+                await s3_client.upload_fileobj(
+                    Fileobj=file_obj,
+                    Bucket=self.bucket_name,
+                    Key=key,
+                    ExtraArgs={'ContentType': 'image/jpeg'},
+                )
             public_url = f"{self.public_endpoint}/{key}"
             return key, public_url
         except Exception:
@@ -41,27 +46,38 @@ class CloudflareR2Storage:
 
     async def delete_image(self, key: str):
         try:
-            self.s3_client.delete_object(
-                Bucket=self.bucket_name,
-                Key=key
-            )
+            async with self.session.client(
+                's3',
+                endpoint_url=self.endpoint_url,
+                aws_access_key_id=self.access_key_id,
+                aws_secret_access_key=self.secret_access_key,
+                config=self.client_config,
+            ) as s3_client:
+                await s3_client.delete_object(Bucket=self.bucket_name, Key=key)
         except Exception:
             log.exception("Failed to delete image")
 
     async def delete_user_images(self, user_uid: str):
         prefix = f"images/{user_uid}/"
         try:
-            paginator = self.s3_client.get_paginator('list_objects_v2')
-            pages = paginator.paginate(Bucket=self.bucket_name, Prefix=prefix)
-            delete_us = []
-            for page in pages:
-                if 'Contents' in page:
-                    for obj in page['Contents']:
-                        delete_us.append({'Key': obj['Key']})
-            if delete_us:
-                self.s3_client.delete_objects(
-                    Bucket=self.bucket_name,
-                    Delete={'Objects': delete_us}
-                )
+            async with self.session.client(
+                's3',
+                endpoint_url=self.endpoint_url,
+                aws_access_key_id=self.access_key_id,
+                aws_secret_access_key=self.secret_access_key,
+                config=self.client_config,
+            ) as s3_client:
+                paginator = s3_client.get_paginator('list_objects_v2')
+                async for page in paginator.paginate(
+                    Bucket=self.bucket_name, Prefix=prefix
+                ):
+                    delete_us = []
+                    if 'Contents' in page:
+                        for obj in page['Contents']:
+                            delete_us.append({'Key': obj['Key']})
+                    if delete_us:
+                        await s3_client.delete_objects(
+                            Bucket=self.bucket_name, Delete={'Objects': delete_us}
+                        )
         except Exception:
             log.exception("Failed to delete user images")
